5d5bff5bf164a1720357e890c1a08d6a8ed7196c
vetnem1lk <klimentev.vlad@gmail.com>
2022-05-17 14:54:14 +0700

    add book window

--- ARMLibrary/MVVM/View/AddBookWindow.xaml.cs
+++ ARMLibrary/MVVM/View/AddBookWindow.xaml.cs
@@ -10,26 +10,30 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ARMLibrary.MVVM.View
 {
     /// <summary>
-    /// Логика взаимодействия для HomeView.xaml
+    /// Логика взаимодействия для AddBookWindow.xaml
     /// </summary>
-    public partial class HomeView : UserControl
+    public partial class AddBookWindow : Window
     {
-        public HomeView()
+        public AddBookWindow()
         {
             InitializeComponent();
-           
         }
 
-        private void AddBook_MLBDown(object sender, MouseButtonEventArgs e)
+        private void ExBtn_MLBDown(object sender, MouseButtonEventArgs e)
         {
-            AddBookWindow addBookWindow = new AddBookWindow();
-            addBookWindow.Show();
+            Close();
+        }
+
+        private void ToolBart_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
+            
         }
     }
 }
---
+++ ARMLibrary/MVVM/ViewModel/AddBookViewModel.cs
@@ -0,0 +1,35 @@
+﻿using ARMLibrary.Core;
+using ARMLibrary.MVVM.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARMLibrary.MVVM.ViewModel
+{
+    class AddBookViewModel : ObservableObject
+    {
+        private string _text;
+        public string CurrentText
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                _text = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        AddBookWindow win = new AddBookWindow();
+        
+        public AddBookViewModel()
+        {
+            string curText = win.InputgNameBook.Text;
+            CurrentText = curText;
+        }
+    }
+}
